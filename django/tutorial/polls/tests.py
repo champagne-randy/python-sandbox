e4c10c86613d87bca57103ab02a86e94e27db3dd
Randy Champagne <r-champagne@neiu.edu>
2016-11-16 02:34:05 -0500

    DetailView no longer displays future questions

--- django/tutorial/polls/tests.py
+++ django/tutorial/polls/tests.py
@@ -60,6 +60,7 @@ class QuestionViewTests(TestCase):
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
+
     def test_index_view_with_a_past_question(self):
         """
         Questions with a pub_date in the past should be displayed on the
@@ -72,6 +73,7 @@ class QuestionViewTests(TestCase):
             ['<Question: Past question.>']
         )
 
+
     def test_index_view_with_a_future_question(self):
         """
         Questions with a pub_date in the future should not be displayed on
@@ -81,6 +83,7 @@ class QuestionViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
 
     def test_index_view_with_future_question_and_past_question(self):
         """
@@ -95,6 +98,7 @@ class QuestionViewTests(TestCase):
             ['<Question: Past question.>']
         )
 
+
     def test_index_view_with_two_past_questions(self):
         """
         The questions index page may display multiple questions.
@@ -106,6 +110,29 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
 
 
 
