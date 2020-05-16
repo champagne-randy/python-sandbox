97bd3f0de0d49cfb5e0021ea473f83519c3420e7
Randy Champagne <champagne.randy@gmail.com>
2020-05-15 22:48:37 -0400

    basic bar chart

--- plotly/app.py
+++ plotly/app.py
@@ -9,7 +9,25 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash')
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5],
+                    'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
 
