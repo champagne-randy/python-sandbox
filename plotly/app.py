d375c8f52cd22b6709939e31b430188beb8c93c4
Randy Champagne <champagne.randy@gmail.com>
2020-05-15 22:42:50 -0400

    bootstrap dash

---
+++ plotly/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash')
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
